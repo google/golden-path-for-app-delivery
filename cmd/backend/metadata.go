9bec533becc3dae5d7e2d843164aa4fd2caf4156
Vic Iglesias <viglesias@google.com>
2018-07-09 15:47:33 -0700

    Fix tests

--- cmd/backend/metadata.go
+++ cmd/backend/metadata.go
@@ -24,35 +24,43 @@ func (i *InstanceMetadata) Populate(version string) {
 	var err error
 	if !metadata.OnGCE() {
 		i.Error = "Not running on GCE"
+		return
 	}
 
 	i.ID, err = metadata.InstanceID()
 	if err != nil {
 		i.Error += "Unable to populate InstanceID\n"
+		return
 	}
 	i.Zone, err = metadata.Zone()
 	if err != nil {
 		i.Error += "Unable to populate Zone\n"
+		return
 	}
 	i.Name, err = metadata.InstanceName()
 	if err != nil {
 		i.Error += "Unable to populate Instance Name\n"
+		return
 	}
 	i.Hostname, err = metadata.Hostname()
 	if err != nil {
 		i.Error += "Unable to populate Hostname\n"
+		return
 	}
 	i.Project, err = metadata.ProjectID()
 	if err != nil {
 		i.Error += "Unable to populate Project\n"
+		return
 	}
 	i.InternalIP, err = metadata.InternalIP()
 	if err != nil {
 		i.Error += "Unable to populate InternalIP\n"
+		return
 	}
 	i.ExternalIP, err = metadata.ExternalIP()
 	if err != nil {
 		i.Error += "Unable to populate ExternalIP\n"
+		return
 	}
 	i.Version = version
 }
