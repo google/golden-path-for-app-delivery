3999540892051c2539aa23d19c854f1cce157e44
Vic Iglesias <viglesias@google.com>
2021-03-23 14:56:38 -0700

    Deref image repos and set minikube docker daemon

--- hack/scan.sh
+++ hack/scan.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -xe
+if [[ $(kubectl config current-context) == "minikube1" ]];then
+  eval $(minikube -p minikube docker-env)
+fi
+
 export SCAN=$(gcloud beta artifacts docker images scan $1 --format json | jq -r .response.scan)
 gcloud beta artifacts docker images list-vulnerabilities ${SCAN}
