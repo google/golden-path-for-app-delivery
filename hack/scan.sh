ea1e77aa988e4f815d74512df275389d1b4cf228
Vic Iglesias <viglesiasce@gmail.com>
2021-04-13 16:15:35 -0700

    Write out scan results

--- hack/scan.sh
+++ hack/scan.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -xe
 export SCAN=$(gcloud beta artifacts docker images scan $1 --format json | jq -r .response.scan)
-gcloud beta artifacts docker images list-vulnerabilities ${SCAN}
+gcloud beta artifacts docker images list-vulnerabilities ${SCAN} > hack/scan.yaml
