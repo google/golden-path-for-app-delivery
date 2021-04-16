a83d84c6fc9af0a422e986925552afa2f47bb076
Vic Iglesias <viglesias@google.com>
2021-04-16 16:13:06 -0700

    Fix up test-image organization

--- test-image/validate.sh
+++ test-image/validate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
 kustomize build cmd/frontend/k8s/prod > frontend-prod.yaml
 kustomize build cmd/backend/k8s/prod >  backend-prod.yaml
-conftest test -p hack/policy ./*-prod.yaml
+conftest test -p test-image/policy ./*-prod.yaml
