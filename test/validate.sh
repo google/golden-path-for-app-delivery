995972b760452bb7cd5d64b4409f9fe8bc31b3a1
Vic Iglesias <viglesias@google.com>
2021-04-22 13:40:43 -0700

    End-to-end first light

--- test/validate.sh
+++ test/validate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -xe
-kustomize build src/frontend/k8s/prod > frontend-prod.yaml
-kustomize build src/backend/k8s/prod >  backend-prod.yaml
-conftest test -p policy ./*-prod.yaml
+cd frontend
+skaffold render -p prod > ../prod.yaml
+cd ..
+conftest test -p policy ./prod.yaml
